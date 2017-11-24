f6660b73b978a271b0e56c17a7c288f6ea686607
gupta.arpit03 <gupta.arpit03@gmail.com>
2017-11-24 14:46:55 +0530

    Database name changed

--- model/mongo.js
+++ model/mongo.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-var dbUrl = process.env.OPENSHIFT_MONGODB_DB_URL+'itargapp1';
+var dbUrl = process.env.OPENSHIFT_MONGODB_DB_URL+'admin_coder';
 mongoose.connect(dbUrl);
  
 console.log("Connected to Mongo Database !!!")

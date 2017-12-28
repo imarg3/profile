92fbe0721ca41c704c5a703b3c0bffb942af88e0
gupta.arpit03 <gupta.arpit03@gmail.com>
2017-12-28 23:33:56 +0530

    Static resources added !!!

--- server.js
+++ server.js
@@ -2,11 +2,13 @@
 var express = require('express'),
     app     = express(),
     morgan  = require('morgan');
-    
+    path       = require('path');
+	
 Object.assign=require('object-assign')
 
 app.engine('html', require('ejs').renderFile);
-app.use(morgan('combined'))
+app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',

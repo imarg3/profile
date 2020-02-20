578ff49ec1f8079808ddd69be3b9d006c234511e
gupta.arpit03 <gupta.arpit03@gmail.com>
2020-02-21 02:40:17 +0530

    Added Accolades and Work Area pages. New HTTP GET handler index route added.

--- server.js
+++ server.js
@@ -80,7 +80,7 @@ app.get('/', function (req, res) {
   }
 });
 
-app.get('/index', function (req, res) {
+app.get('/profile', function (req, res) {
   // try to initialize the db on every request if it's not already
   // initialized.
   if (!db) {
